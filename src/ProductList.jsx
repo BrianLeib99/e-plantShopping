5c62bbc9f3eb7e391f29fc0990691356ca9e8f72
BrianLeib99 <brian.l039930@gmail.com>
2025-05-02 14:22:19 -0400

    final update

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
 import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
-    const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({});
+    const [setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const [setAddedToCart] = useState({});
     const dispatch = useDispatch();
     const cartItems = useSelector((state) => state.cart.items);
     const cartCount = cartItems.reduce((total, item) => total + item.quantity, 0);
@@ -291,12 +291,12 @@ function ProductList({ onHomeClick }) {
                                 <circle cx="184" cy="216" r="12"></circle>
                                 <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
                             </svg>
-                        {cartCount > 0 && (
+                            {cartCount > 0 && (
                             <span style={{
                                 position: 'absolute',
                                 top: '0px',
                                 right: '0px',
-                                background: 'red',
+                                background: '#e74c3c',
                                 color: 'white',
                                 borderRadius: '50%',
                                 padding: '4px 8px',
@@ -304,7 +304,7 @@ function ProductList({ onHomeClick }) {
                             }}>
                             {cartCount}
                             </span>
-                        )}
+                            )}
                         </h1>
                         </a>
                     </div>
